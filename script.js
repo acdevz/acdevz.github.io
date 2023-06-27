56003474223803d58c73130e008b150577246b8c
acdevs <amanchandra.in@gmail.com>
2023-06-27 21:57:22 +0530

    profiles deleted.

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function fadeMe(id){
 var body=document.getElementsByTagName('body')[0].style; 
 function myFunction() {
     var scroll =((wrap.scrollTop) / (wrap.scrollHeight));
-    let x = 1/14;
+    let x = 1/8;
     if(scroll<1.5*x){
         body.backgroundColor='rgb(107, 175, 146)';
         activate_icon(0);
@@ -40,26 +40,26 @@ function myFunction() {
         body.backgroundColor='rgb(140, 114, 178)';
         activate_icon(1);
         fadeMe(1);
+    // }else if(scroll<5.5*x){
+    //     body.backgroundColor='rgb(225, 92, 127)';
+    //     activate_icon(2);
+    //     fadeMe(2);
+    // }else if(scroll<7.5*x){
+    //     body.backgroundColor='rgba(225, 222, 0, 0.7)';
+    //     activate_icon(3);
+    //     fadeMe(3);
     }else if(scroll<5.5*x){
-        body.backgroundColor='rgb(225, 92, 127)';
+        body.backgroundColor='rgb(39, 171, 252)';
         activate_icon(2);
         fadeMe(2);
+    // }else if(scroll<11.5*x){
+    //     body.backgroundColor='rgb(218, 102, 108)';
+    //     activate_icon(5);
+    //     fadeMe(5);
     }else if(scroll<7.5*x){
-        body.backgroundColor='rgba(225, 222, 0, 0.7)';
+        body.backgroundColor='rgb(34, 138, 188)';
         activate_icon(3);
         fadeMe(3);
-    }else if(scroll<9.5*x){
-        body.backgroundColor='rgb(39, 171, 252)';
-        activate_icon(4);
-        fadeMe(4);
-    }else if(scroll<11.5*x){
-        body.backgroundColor='rgb(218, 102, 108)';
-        activate_icon(5);
-        fadeMe(5);
-    }else if(scroll<13.5*x){
-        body.backgroundColor='rgb(34, 138, 188)';
-        activate_icon(6);
-        fadeMe(6);
     }
 }
 
